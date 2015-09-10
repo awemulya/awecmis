e1de9d587fb91e4679cc1a57093ffd8d0d4ccca5
awemulya <aweamulya@gmail.com>
2015-09-10 14:19:39 +0545

    models

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+from google.appengine.ext import ndb
 
-# Create your models here.
+DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
+
+
+def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
+    """Constructs a Datastore key for a Guestbook entity.
+
+    We use guestbook_name as the key.
+    """
+    return ndb.Key('Guestbook', guestbook_name)
+
+
+class Author(ndb.Model):
+    """Sub model for representing an author."""
+    identity = ndb.StringProperty(indexed=False)
+    email = ndb.StringProperty(indexed=False)
+
+
+class Greeting(ndb.Model):
+    """A main model for representing an individual Guestbook entry."""
+    author = ndb.StructuredProperty(Author)
+    content = ndb.StringProperty(indexed=False)
+    date = ndb.DateTimeProperty(auto_now_add=True)
--- employee/views.py
+++ employee/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from employee.models import DEFAULT_GUESTBOOK_NAME, Greeting, guestbook_key, Author
+
 
 def employee(request):
-    return render(request, "employee.html")
+    p = Author(identity='Arthur Dentee', email="dentee@gmail")
+    p.put()
+    g = Greeting(author=p, content="dent@gmail")
+    g.put()
+    gr = Greeting()
+    greetings = gr.populate(author=p)
+
+    # guestbook_name = request.GET.get('guestbook_name', DEFAULT_GUESTBOOK_NAME)
+    # greetings_query = Greeting.query(
+    #     ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
+    # greetings = greetings_query.fetch(10)
+    return render(request, "employee.html", {'objlist': greetings})
